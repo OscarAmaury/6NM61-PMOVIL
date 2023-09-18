6ee28ebbced1675a631dbf6192e04488351aeafc
OscarAmaury <144569816+OscarAmaury@users.noreply.github.com>
2023-09-17 19:37:14 -0600

    Add files via upload

---
+++ PRACTICA-01-Alternativa_Online_para_Testeo_Rapido.kt
@@ -0,0 +1,15 @@
+fun main() {
+    // 1.- Imprimir el mismo mensaje que imprimimos en la practica "HelloUPIICSA"
+    print("Hello UPIICSA")
+
+    // 2.- Abajo de la linea de código de la tarea
+    //          2.1.- Poner otro mensaje en consola ¿Qué pasa con los dos mensajes?
+    print("Mi Nombre es Oscar")
+    //          R= Lo que pasa es que salen los mensajes juntos.
+
+    // 3.- Ahora utiliza println e imprime alguna otra cosa, ¿Qué diferencia hay con print?
+    println("")
+    println("Hello UPIICSA")
+    println("Mi Nombre es Oscar")
+    //          R= La diferencia es que ya hay un salto de linea.
+}

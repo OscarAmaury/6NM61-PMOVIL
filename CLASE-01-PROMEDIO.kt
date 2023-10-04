48755d5d502779f92563e086837f968a7457e786
OscarAmaury <144569816+OscarAmaury@users.noreply.github.com>
2023-10-03 20:25:30 -0600

    Add files via upload

---
+++ CLASE-01-PROMEDIO.kt
@@ -0,0 +1,15 @@
+fun main() {
+    //Declraramos las calificaciones.
+    val Calificacion1 = 8
+    val Calificacion2 = 8
+    val Calificacion3 = 10
+
+    //Hacemos la operacion para sacar el promedio.
+    val Promedio = (Calificacion1 + Calificacion2 + Calificacion3) / 3.0
+
+    //Imprimimos las calificaciones y el resultado del promedio en pantalla.
+    println("Calificación 1: $Calificacion1")
+    println("Calificación 2: $Calificacion2")
+    println("Calificación 3: $Calificacion3")
+    println("Promedio: $Promedio")
+}
